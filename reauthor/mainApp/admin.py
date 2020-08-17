bde25cdd5d1fd2967f55a90e99157d9cc003756e
shalo1040 <kimd02@pfw.edu>
2020-08-17 12:13:04 +0900

    models.py(Profile, Toon, Characters, CharactersPosition, Restory)

--- reauthor/mainApp/admin.py
+++ reauthor/mainApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Toon, Restory, Characters, CharactersPosition
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Toon)
+admin.site.register(Restory)
+admin.site.register(Characters)
+admin.site.register(CharactersPosition)
--- reauthor/mainApp/models.py
+++ reauthor/mainApp/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    isAuthor = models.BooleanField(default=False)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+#등록된 작품 정보
+class Toon(models.Model):
+    userID = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=30)
+    genre = (models.CharField(max_length=30))
+    url = models.TextField()
+    detail = models.TextField()
+    thumbnail = models.ImageField(upload_to='images/')
+
+#회차 정보
+class Restory(models.Model):
+    toonID = models.ForeignKey(Toon, on_delete=models.CASCADE)
+    chapterNum = models.IntegerField()
+    detail = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField(default=timezone.now)
+
+#작품 속 등장인물
+class Characters(models.Model):
+    toonID = models.ForeignKey(Toon, on_delete=models.CASCADE)
+    profile = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=30)
+    detail = models.TextField()
+
+#회차별 등장인물 위치 값
+class CharactersPosition(models.Model):
+    toonID = models.ForeignKey(Toon, on_delete=models.CASCADE)
+    chapterID = models.ForeignKey(Restory, on_delete=models.CASCADE, related_name="restory")
+    posX = models.FloatField()
+    posY = models.FloatField()
